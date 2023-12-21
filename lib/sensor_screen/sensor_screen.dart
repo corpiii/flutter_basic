d4cb0bb57b022bebb9857ecb12b16611cf9a1006
corpi <lj845854@gmail.com>
2023-12-21 15:54:24 +0900

    feat: 휴대폰의 가로모드만 지원하도록 설정

--- lib/sensor_screen/sensor_screen.dart
+++ lib/sensor_screen/sensor_screen.dart
@@ -1,29 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 
 class SensorScreen extends StatelessWidget {
-
-
   const SensorScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    //
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
+
     final centerX = MediaQuery.of(context).size.width / 2 - 50;
     final centerY = MediaQuery.of(context).size.height / 2 - 50;
 
     return Scaffold(
       body: Stack(
         children: [
-          Positioned(
-            top: centerY,
-            left: centerX,
-            child: Container(
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.green,
-              ),
-              width: 100,
-              height: 100,
-            ),
+          StreamBuilder<AccelerometerEvent>(
+            stream: accelerometerEventStream(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+
+              final event = snapshot.data!;
+              List<double> accelerometerValues = [event.x, event.y, event.z];
+              print(accelerometerValues);
+
+              return Positioned(
+                top: centerY,
+                left: centerX,
+                child: Container(
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.green,
+                  ),
+                  width: 100,
+                  height: 100,
+                ),
+              );
+            }
           )
         ],
       ),
