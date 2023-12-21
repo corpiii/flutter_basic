5a755e70d056338181d51345dc60280424deb33e
corpi <lj845854@gmail.com>
2023-12-21 21:08:16 +0900

    style: 파일명 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_basic/xylophone_screen/main_screen.dart';
+import 'package:flutter_basic/xylophone_screen/xylophone_screen.dart';
 
 void main() {
   runApp(const MyApp());
