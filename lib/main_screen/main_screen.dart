98420c8127d6c447053103bb71f9e7cfb5a583e3
corpi <lj845854@gmail.com>
2023-12-21 15:01:05 +0900

    feat: image의 readAsBytes를 사용하기위해 FutureBuilder사용 및 이를 PageView로 감싸 슬라이드.

--- lib/main_screen/main_screen.dart
+++ lib/main_screen/main_screen.dart
@@ -1,10 +1,60 @@
-import 'package:flutter/material.dart';
+import 'dart:typed_data';
 
-class MainScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
   @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final ImagePicker imagePicker = ImagePicker();
+  late List<XFile> images;
+
+  @override
+  void initState() {
+    super.initState();
+    loadImages();
+  }
+
+  Future<void> loadImages() async {
+    images = await imagePicker.pickMultiImage();
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('전자액자'),
+      ),
+      body: images == null
+          ? const Center(
+              child: Text('No Data'),
+            )
+          : PageView(
+              children: images!.map((e) {
+                return FutureBuilder<Uint8List>(
+                    future: e.readAsBytes(),
+                    builder: (context, snapshot) {
+                      final data = snapshot.data;
+
+                      if (data == null ||
+                          snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                      return Image.memory(
+                        data,
+                        width: double.infinity,
+                      );
+                    });
+              }).toList(),
+            ),
+    );
   }
 }
