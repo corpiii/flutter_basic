c575d0b736b44d738b5b2c24f5ccaad49b30c30f
corpi <lj845854@gmail.com>
2023-12-22 10:04:09 +0900

    feat: TitleAppBar 생성

--- lib/main_screen/main_screen.dart
+++ lib/main_screen/main_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_basic/main_screen/title_app_bar.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+
+          TitleAppBar(),
+
+        ],
+      )
+    );
   }
 }
---
+++ lib/main_screen/title_app_bar.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TitleAppBar extends StatelessWidget {
+  const TitleAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.deepPurple,
+      expandedHeight: 300,
+
+    );
+  }
+}
