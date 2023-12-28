9e8227cd3a80ba32c24a683a6977af2106a012e0
corpi <lj845854@gmail.com>
2023-12-28 15:34:12 +0900

    feat: FutureBuilder를 통해 레포지토리에서 받은 리스트를 각 리스트뷰로 변환

--- lib/ui/main_screen/main_screen.dart
+++ lib/ui/main_screen/main_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_basic/di/album_repository_provider.dart';
 import 'package:flutter_basic/repository/interface/album_repository.dart';
 
+import '../../model/album.dart';
+
 class MainScreen extends StatelessWidget {
   final AlbumRepository _repository = albumRepository;
 
@@ -9,27 +11,36 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    _repository.fetchAllAlbums();
     return Scaffold(
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: ListView.builder(
-            itemCount: 3,
-            itemBuilder: (BuildContext context, int index) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12.0),
-                    color: Colors.deepPurple,
-                  ),
-                  width: double.infinity,
-                  height: MediaQuery.of(context).size.height / 10,
-                ),
-              );
-            },
-          ),
+          child: FutureBuilder<List<Album>>(
+              future: _repository.fetchAllAlbums(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData == false) {
+                  return Center(child: CircularProgressIndicator());
+                }
+
+                List<Album> models = snapshot.data!;
+
+                return ListView.builder(
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12.0),
+                          color: Colors.deepPurple,
+                        ),
+                        width: double.infinity,
+                        height: MediaQuery.of(context).size.height / 10,
+                        child: Text(models[index].title),
+                      ),
+                    );
+                  },
+                );
+              }),
         ),
       ),
     );
